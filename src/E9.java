139af33d3604695059765dc11019b27e3359797d
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-02 21:29:11 -0500

    update

--- src/E9.java
+++ src/E9.java
@@ -5,27 +5,26 @@ public class E9 {
 
     public static void main(String[] args) {
 
-        int cant;
+        int cant, N;
+        Random rnd = new Random();
         Scanner scn = new Scanner(System.in);
         System.out.print("Ingrese el tamano de su arreglo: ");
         cant=scn.nextInt();
         int[] arr = new int[cant];
 
-        for (int i=0; i<9; i++){
-            for (int j=0; j<9; j++){
-                if (i==4 || j==4){
-                    patron[i][j]="o";
-                } else {
-                    patron[i][j]="-";
-                }
-            }
+        for (int i=0; i<cant; i++){
+            int x = rnd.nextInt(50)+1;
+            arr[i]=x;
         }
 
-        for (int i=0; i<9; i++){
-            for (int j=0; j<9; j++){
-                System.out.print(patron[i][j] + "  " );
-            }
-            System.out.println();
+        System.out.println(Arrays.toString(arr));
+        System.out.print("Ingrese el numero a buscar en el arreglo: ");
+        N=scn.nextInt();
+
+        if (Arrays.stream(arr).anyMatch(i -> i==N)){
+            System.out.println("El numero " + N + " SI esta en el arreglo");
+        } else {
+            System.out.println("El numero " + N + " NO esta en el arreglo");
         }
     }
 }
