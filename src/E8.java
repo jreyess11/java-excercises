b5d6755b576ec8a0ba66362575ae182f16a5d9ea
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-02 21:07:01 -0500

    update

---
+++ src/E8.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+//author Jose Reyes
+public class E8 {
+
+    public static void main(String[] args) {
+
+        String[][] patron = new String[9][9];
+
+        for (int i=0; i<9; i++){
+            for (int j=0; j<9; j++){
+                if (i==4 || j==4){
+                    patron[i][j]="o";
+                } else {
+                    patron[i][j]="-";
+                }
+            }
+        }
+
+        for (int i=0; i<9; i++){
+            for (int j=0; j<9; j++){
+                System.out.print(patron[i][j] + "  " );
+            }
+            System.out.println();
+        }
+    }
+}
