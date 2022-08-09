81f3af38a93df82291f364192eba9c2242fb2076
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-09 15:27:56 -0500

    up

--- src/E10.java
+++ src/E10.java
@@ -1,30 +1,70 @@
+import java.text.DecimalFormat;
 import java.util.*;
+import java.util.stream.IntStream;
 
 //author Jose Reyes
 public class E10 {
 
+    public static int tarifa(int num){
+        int COP = 0;
+        if (25 < num && num <= 300){
+            COP = + 1500 * num;
+        }
+        if (300 < num && num <= 500){
+            COP = + 2500 * num;
+        }
+        return COP;
+    }
+
     public static void main(String[] args) {
 
-        int cant, N;
-        Random rnd = new Random();
+        int N, total = 0, i = 0;
+        DecimalFormat df = new DecimalFormat("#,###.##");
         Scanner scn = new Scanner(System.in);
-        System.out.print("Ingrese el tamano de su arreglo: ");
-        cant=scn.nextInt();
-        int[] arr = new int[cant];
-
-        for (int i=0; i<cant; i++){
-            int x = rnd.nextInt(50)+1;
-            arr[i]=x;
-        }
-
-        System.out.println(Arrays.toString(arr));
-        System.out.print("Ingrese el numero a buscar en el arreglo: ");
+        System.out.print("Ingrese la cantidad de bultos: ");
         N=scn.nextInt();
+        List<Integer> controlpeso = new LinkedList<>();
+        int controlprecio = 0;
 
-        if (Arrays.stream(arr).anyMatch(i -> i==N)){
-            System.out.println("El numero " + N + " SI esta en el arreglo");
-        } else {
-            System.out.println("El numero " + N + " NO esta en el arreglo");
+        while (total < N){
+            System.out.print("Ingrese el peso del bulto " + (i+1) + ": ");
+            int peso = scn.nextInt();
+
+            if (peso > 0){
+                if (peso <= 500){
+                    controlpeso.add(peso);
+                    if (controlpeso.stream().reduce(Integer::sum).get()<=1000){
+                        int C = tarifa(peso);
+                        controlprecio += C;
+                        i++;
+                        total +=1;
+                    } else {
+                        controlpeso.remove(i);
+                        System.out.print("\nLo sentimos, este bulto excede el tamano maximo de carga (18.000 kg).\n" +
+                                "Por el momento su peso es " + controlpeso.stream().reduce(Integer::sum).get() + " kg." +
+                                "\nOprima 1 si desea agregar otro bulto de menor tamano o 0 para terminar la lista: ");
+                        int des = scn.nextInt();
+                        if (des==0){
+                            total = N;
+                        } else {
+                            System.out.println();
+                        }
+                    }
+                } else {
+                    System.out.println("Recuerde que un bulto no puede exceder los 500 kg");
+                }
+            } else {
+                System.out.println("Por favor ingrese un peso valido.");
+            }
         }
+
+        double promedio = controlpeso.stream().reduce(Integer::sum).get() / controlpeso.size();
+        double U = controlprecio/4337;
+        System.out.println("\nEl peso final de la carga es "+ controlpeso.stream().reduce(Integer::sum).get() + " kg, con un total de " + controlpeso.size() +" bultos.");
+        System.out.println("El peso del bulto mas pesado es " + Collections.max(controlpeso) + " kg y del menor es " + Collections.min(controlpeso) + " kg.");
+        System.out.println("El peso promedio de los bultos es "+ df.format(promedio)+" kg.");
+        System.out.println("El precio final de la carga es COP $"+ df.format(controlprecio) +" // USD $"+ df.format(U));
+
+
     }
 }
